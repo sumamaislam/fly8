2a5893114b9120b3b5e1cd726ece3e37b9b30150
sumamaislam <sumamaislam800@gmail.com>
2022-12-05 22:47:31 +0500

    okk

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -78,10 +78,210 @@ function Header() {
             </div>
           </div>
         </div>
-        <div className="hidden lg:block pt-[50px] py-[30px]">
+        <div className="hidden  lg:block pt-[50px] py-[30px]">
           <ul className="flex gap-8 justify-center font-semibold text-[#0AA085]  ">
+        <div class="group inline-block">
+      <button
+        aria-haspopup="true"
+        aria-controls="menu"
+        class="outline-none focus:outline-none px-3 py-1 bg-white rounded-sm flex items-center "
+      >
+        <span class=" font-semibold w-fit text-[18px]">Sale</span>
+        <span>
+          <svg
+            class="fill-current h-4 w-4 transform group-hover:-rotate-180
+        transition duration-150 ease-in-out"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+            />
+          </svg>
+        </span>
+      </button>
+      <ul
+        id="menu"
+        aria-hidden="true"
+        class="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
+  transition duration-150 ease-in-out origin-top min-w-[200px] cursor-pointer"
+      >
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">Programming</li>
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">D11</li>
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">HHC</li>
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">MASTERBLEND</li>
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">TRIPLE TAP </li>
+        <li class="rounded-sm relative px-3 py-2 hover:bg-gray-100">
+          <button
+            aria-haspopup="true"
+            aria-controls="menu-lang"
+            class="w-full text-left flex items-center outline-none focus:outline-none"
+          >
+            <span class="pr-1 flex-1">Langauges</span>
+            <span class="mr-auto">
+              <svg
+                class="fill-current h-4 w-4
+            transition duration-150 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                />
+              </svg>
+            </span>
+          </button>
+          <ul
+            id="menu-lang"
+            aria-hidden="true"
+            class="bg-white border rounded-sm absolute top-0 right-0 
+  transition duration-150 ease-in-out origin-top-left
+  min-w-[200px]
+  "
+          >
+            <li class="px-3 py-2 hover:bg-gray-100">Javascript</li>
+            <li class="rounded-sm relative px-3 py-2 hover:bg-gray-100">
+              <button
+                aria-haspopup="true"
+                aria-controls="menu-lang-python"
+                class="w-full text-left flex items-center outline-none focus:outline-none"
+              >
+                <span class="pr-1 flex-1">Python</span>
+                <span class="mr-auto">
+                  <svg
+                    class="fill-current h-4 w-4
+                transition duration-150 ease-in-out"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                    />
+                  </svg>
+                </span>
+              </button>
+              <ul
+                id="menu-lang-python"
+                aria-hidden="true"
+                class="bg-white border rounded-sm absolute top-0 right-0 
+      transition duration-150 ease-in-out origin-top-left
+      min-w-[200px]
+      "
+              >
+                <li class="px-3 py-2 hover:bg-gray-100">vape</li>
+                <li class="px-3 py-2 hover:bg-gray-100">vape 1</li>
+                <li class="px-3 py-2 hover:bg-gray-100">vape 2</li>
+                <li class="px-3 py-2 hover:bg-gray-100">vape 3</li>
+                <li class="px-3 py-2 hover:bg-gray-100">vape 4</li>
+                <li class="px-3 py-2 hover:bg-gray-100">vape 5</li>
+              </ul>
+            </li>
+            <li class="px-3 py-2 hover:bg-gray-100">Go</li>
+            <li class="px-3 py-2 hover:bg-gray-100">Rust</li>
+          </ul>
+        </li>
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">Testing</li>
+      </ul>
+    </div>
+            
             <Link href="/">
-              <li>SALE</li>
+            <div class="group inline-block">
+      <button
+        aria-haspopup="true"
+        aria-controls="menu"
+        class="outline-none focus:outline-none px-3 py-1 bg-white rounded-sm flex items-center "
+      >
+        <span class="pr-1 font-semibold flex-1 text-[18px]">Sale</span>
+        <span>
+          <svg
+            class="fill-current h-4 w-4 transform group-hover:-rotate-180
+        transition duration-150 ease-in-out"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+            />
+          </svg>
+        </span>
+      </button>
+      <ul
+        id="menu"
+        aria-hidden="true"
+        class="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
+  transition duration-150 ease-in-out origin-top min-w-[150px]"
+      >
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">Programming</li>
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">DevOps</li>
+        <li class="rounded-sm relative px-3 py-2 hover:bg-gray-100">
+          <button
+            aria-haspopup="true"
+            aria-controls="menu-lang"
+            class="w-full text-left flex items-center outline-none focus:outline-none"
+          >
+            <span class="pr-1 flex-1">Langauges</span>
+            <span class="mr-auto">
+              <svg
+                class="fill-current h-4 w-4
+            transition duration-150 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                />
+              </svg>
+            </span>
+          </button>
+          <ul
+            id="menu-lang"
+            aria-hidden="true"
+            class="bg-white border rounded-sm absolute top-0 right-0 
+  transition duration-150 ease-in-out origin-top-left
+  min-w-[150px]
+  "
+          >
+            <li class="px-3 py-1 hover:bg-gray-100">Javascript</li>
+            <li class="rounded-sm relative px-3 py-1 hover:bg-gray-100">
+              <button
+                aria-haspopup="true"
+                aria-controls="menu-lang-python"
+                class="w-full text-left flex items-center outline-none focus:outline-none"
+              >
+                <span class="pr-1 flex-1">Python</span>
+                <span class="mr-auto">
+                  <svg
+                    class="fill-current h-4 w-4
+                transition duration-150 ease-in-out"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                    />
+                  </svg>
+                </span>
+              </button>
+              <ul
+                id="menu-lang-python"
+                aria-hidden="true"
+                class="bg-white border rounded-sm absolute top-0 right-0 
+      transition duration-150 ease-in-out origin-top-left
+      min-w-[150px]
+      "
+              >
+                <li class="px-3 py-2 hover:bg-gray-100">2.7</li>
+                <li class="px-3 py-2 hover:bg-gray-100">3+</li>
+              </ul>
+            </li>
+            <li class="px-3 py-2 hover:bg-gray-100">Go</li>
+            <li class="px-3 py-2 hover:bg-gray-100">Rust</li>
+          </ul>
+        </li>
+        <li class="rounded-sm px-3 py-2 hover:bg-gray-100">Testing</li>
+      </ul>
+    </div>
+              
             </Link>
             <Link href="/">
               <li>LIVE RESIN</li>
