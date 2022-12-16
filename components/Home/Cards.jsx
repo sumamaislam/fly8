12d7fbe3fe69fdd3cd7b934c0dd1f9a54c87d2dc
sumamaislam <sumamaislam800@gmail.com>
2022-12-16 19:32:40 +0500

    abcd

--- components/Home/Cards.jsx
+++ components/Home/Cards.jsx
@@ -11,7 +11,7 @@ function Cards() {
   const router = useRouter();
   const [btnText, setBtnText] = useState(["ADD To Cart", "ADDED To Cart"]);
   const { vapes } = useSelector((state) => state.home);
-  
+
   const handleAdd = (data) => {
     dispatch(addToCarts(data));
     // router.push({ pathname: "/add_to_cart" });
@@ -41,18 +41,18 @@ function Cards() {
                 </div>
                 <div className="pt-[15px]">
                   <h1 className="text-[18px] xl:text-[19px] font-extrabold  text-[black] dark:text-white text-center">
-                    {items.name}
+                    {items.title}
                   </h1>
                   <h5 className=" text-[14px]  xl:text-[14px]  font-normal  pt-[15px] text-[#535353]  text-center ">
-                    {items.title}
+                    {items.details}
                   </h5>
                   <div className="flex justify-center mt-[42px]">
                     <Link
                       href={`${
-                        (items.id === 1 && "/delta11") ||
-                        (items.id === 2 && "/cards") ||
-                        (items.id === 3 && "/masterblend") ||
-                        (items.id === 4 && "/hhc")
+                        (index === 0 && "/delta11") ||
+                        (index === 1 && "/cards") ||
+                        (index === 2 && "/masterblend") ||
+                        (index === 3 && "/hhc")
                       }`}
                       className=" items-center hover:animate-bounce text-[14px] xl:text-[18px] font-[700px] rounded-[8px] shadow-md bg-black text-center px-4 py-3 text-[white]  outline-none cursor-pointer  hover:text-white "
                       onClick={() => handleAdd(items)}
--- components/Profile/Subscription.jsx
+++ components/Profile/Subscription.jsx
@@ -2,173 +2,34 @@ import React from "react";
 import { useState } from "react";
 
 function Subscription() {
-  const [offers, setOffers] = useState();
+  const [offers, setOffers] = useState(false);
   return (
     <div className="container px-10 m-auto ">
-     <div className="text-2xl font-bold tetx-black text-center mt-[60px] text-red-600">
-      <p>SUBSCRIBED PRODUCTS</p>
-     </div>
-{/*    
-        <div className="flex gap-4 justify-center mt-[50px] ">
-          <div>
-            <img className="h-24 w-24" src="/images/p1.jpeg" alt="" />
-          </div>
-          <div>
-            <div className="flex gap-4 items-center">
-              <p className="text-[12px] font-bold">
-                DELTA-9O HIGH POTENCY VAPE{" "}
-                <span className="text-[12px]">WITH 2 PACK COMBO</span>{" "}
-              </p>
-              <div>
-                <p>
-                  Billing : <span>$34.56</span>
-                </p>
-              </div>
-            </div>
-
-            <div className="flex   justify-between mt-[10px]">
-              <div className=" border rounded-md bg-[#CEF1CD]">
-                <div className="flex justify-between gap-12 w-full h-[25px]  px-2 items-center  rounded-md ">
-                  <div className="flex gap-3">
-                    <div className="flex items-center">
-                      <input
-                        id="country-option-1"
-                        type="radio"
-                        checked={offers}
-                        name="countries"
-                        value="USA"
-                        className="h-4 w-4 border-gray-300  focus:ring-black "
-                        aria-labelledby="country-option-1"
-                        aria-describedby="country-option-1"
-                        onClick={() => setOffers(true)}
-                      />
-                    </div>
-                    <div className="uppercase">
-                      <p className="md:text-[12px] text-[12px]  font-normal">
-                        EVERY 2 WEEKS
-                      </p>
-                    </div>
-                  </div>
-
-                  <div>
-                    <p className="text-[12px]  font-bold">$25.55</p>
-                  </div>
-                </div>
-                
-                {offers && (
-                  <div className="px-2  pb-[15px] ">
-                    <div className="flex gap-3">
-                      <div className="flex items-center">
-                        <input
-                          id="country-option-1"
-                          type="radio"
-                      
-                          name="countries"
-                          value="USA"
-                          className="h-4 w-4 border-gray-300  focus:ring-black "
-                          aria-labelledby="country-option-1"
-                          aria-describedby="country-option-1"
-                       
-                        />
-                      </div>
-                      <div className="uppercase">
-                        <p className=" text-[12px]  font-normal">
-                          EVERY 2 WEEKS
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex gap-3 pt-2">
-                      <div className="flex items-center">
-                        <input
-                          id="country-option-1"
-                          type="radio"
-                       
-                          name="countries"
-                          value="USA"
-                          className="h-4 w-4 border-gray-300  focus:ring-black "
-                          aria-labelledby="country-option-1"
-                          aria-describedby="country-option-1"
-                         
-                        />
-                      </div>
-                      <div className="uppercase">
-                        <p className=" text-[12px]  font-normal">
-                          EVERY 1 MONTH
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex gap-3 pt-2">
-                      <div className="flex items-center">
-                        <input
-                          id="country-option-1"
-                          type="radio"
-                         
-                          name="countries"
-                          value="USA"
-                          className="h-4 w-4 border-gray-300  focus:ring-black "
-                          aria-labelledby="country-option-1"
-                          aria-describedby="country-option-1"
-                         
-                        />
-                      </div>
-                      <div className="uppercase">
-                        <p className=" text-[12px]  font-normal">
-                          EVERY 6 MONTHS
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex gap-3 pt-2">
-                      <div className="flex items-center">
-                        <input
-                          id="country-option-1"
-                          type="radio"
-                          
-                          name="countries"
-                          value="USA"
-                          className="h-4 w-4 border-gray-300  focus:ring-black "
-                          aria-labelledby="country-option-1"
-                          aria-describedby="country-option-1"   
-                        />
-                      </div>
-                      <div className="uppercase">
-                        <p className=" text-[12px]  font-normal">
-                          EVERY 1 YEAR
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-<div className="flex gap-4">
-
-              <div className="">
-                <button className="text-[10px] bg-black text-white py-2 px-4  rounded-lg">VIEW DETAIL</button>
-              </div>
-              <div className="">
-                <button className="text-[10px] py-2 px-4  text-white bg-red-600 rounded-lg">CANCLE</button>
-              </div>
-</div>
-            </div>
-          </div>
+      <div className="text-2xl font-bold tetx-black text-center mt-[60px] text-red-600">
+        <p>SUBSCRIBED PRODUCTS</p>
+      </div>
+      
+      <div className="flex gap-12 justify-center mt-[50px]">
+        <div className="">
+          <img className="w-[150px] border" src="/images/p1.jpeg" alt="" />
         </div>
-     */}
-     <div className="flex gap-12 justify-center mt-[50px]">
-    <div className="">
-    <img className="w-[150px] border" src="/images/p1.jpeg" alt="" />
-      </div>
-    <div className="">
-      {/* 1 */}
-      <div className="flex gap-12">
-<div>
-  <p className="font-normal text-[18px]">BEST FALAVOR OF THE DELTA-9O HIGH POTENCY VAPE WITH 2 PACK COMBO</p>
-  </div>
-<div>
-  <p className="text-[16px] font-bold">Billing : <span className="text-red-600">$34.56</span></p>
-  </div>
-      </div>
-      {/* 2 */}
-      <div className="flex justify-between mt-6">
-      <div className="flex   justify-between ">
+        <div className="">
+          {/* 1 */}
+          <div className="flex gap-12">
+            <div>
+              <p className="font-normal text-[18px]">
+                BEST FALAVOR OF THE DELTA-9O HIGH POTENCY VAPE WITH 2 PACK COMBO
+              </p>
+            </div>
+            <div>
+              <p className="text-[16px] font-bold">
+                Billing : <span className="text-red-600">$34.56</span>
+              </p>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className="flex justify-between mt-6">
+            <div className="flex   justify-between ">
               <div className=" border rounded-md bg-[#CEF1CD]">
                 <div className="flex justify-between gap-[174px] w-full h-[55px]  px-2 items-center  rounded-md ">
                   <div className="flex gap-3">
@@ -177,12 +38,11 @@ function Subscription() {
                         id="country-option-1"
                         type="radio"
                         checked={offers}
-                        name="countries"
                         value="USA"
                         className="h-4 w-4 border-gray-300  focus:ring-black "
                         aria-labelledby="country-option-1"
                         aria-describedby="country-option-1"
-                        onClick={() => setOffers(true)}
+                        onClick={() => setOffers(!offers)}
                       />
                     </div>
                     <div className="uppercase">
@@ -193,25 +53,21 @@ function Subscription() {
                   </div>
 
                   <div>
-                    <p className="text-[12px] md:text-[16px]  font-bold">$25.55</p>
+                    <p className="text-[12px] md:text-[16px]  font-bold">
+                      $25.55
+                    </p>
                   </div>
                 </div>
-                
+
                 {offers && (
                   <div className="px-2  pb-[15px] ">
-                    
                     <div className="flex gap-3 pt-2">
                       <div className="flex items-center">
                         <input
-                          id="country-option-1"
+                          id="country-option-2"
                           type="radio"
-                       
-                          name="countries"
                           value="USA"
                           className="h-4 w-4 border-gray-300  focus:ring-black "
-                          aria-labelledby="country-option-1"
-                          aria-describedby="country-option-1"
-                         
                         />
                       </div>
                       <div className="uppercase">
@@ -223,15 +79,10 @@ function Subscription() {
                     <div className="flex gap-3 pt-2">
                       <div className="flex items-center">
                         <input
-                          id="country-option-1"
+                          id="country-option-3"
                           type="radio"
-                         
-                          name="countries"
                           value="USA"
                           className="h-4 w-4 border-gray-300  focus:ring-black "
-                          aria-labelledby="country-option-1"
-                          aria-describedby="country-option-1"
-                         
                         />
                       </div>
                       <div className="uppercase">
@@ -243,14 +94,10 @@ function Subscription() {
                     <div className="flex gap-3 pt-2">
                       <div className="flex items-center">
                         <input
-                          id="country-option-1"
+                          id="country-option-4"
                           type="radio"
-                          
-                          name="countries"
                           value="USA"
                           className="h-4 w-4 border-gray-300  focus:ring-black "
-                          aria-labelledby="country-option-1"
-                          aria-describedby="country-option-1"   
                         />
                       </div>
                       <div className="uppercase">
@@ -262,15 +109,22 @@ function Subscription() {
                   </div>
                 )}
               </div>
+            </div>
+            <div className="flex gap-8">
+              <div>
+                <button className="bg-black text-white px-5 py-4 font-bold rounded-xl">
+                  VIEW DETAIL
+                </button>
               </div>
-        <div className="flex gap-8">
-          <div><button className="bg-black text-white px-5 py-4 font-bold rounded-xl">VIEW DETAIL</button></div>
-          <div><button className="bg-red-600 text-white px-5 py-4 font-bold rounded-xl">REMOVE</button></div>
+              <div>
+                <button className="bg-red-600 text-white px-5 py-4 font-bold rounded-xl">
+                  REMOVE
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      </div>  
-     </div>
-      
     </div>
   );
 }
