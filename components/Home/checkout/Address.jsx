12c4b1fb2d2bc258b90ede1dbb6e806b9ea3295a
Muhammad Waseem <biztekapps.dev@gmail.com>
2022-12-29 00:26:22 +0500

    abcddd

--- components/Home/checkout/Address.jsx
+++ components/Home/checkout/Address.jsx
@@ -32,7 +32,7 @@ function Address({ setShow }) {
   const [isProcessing, setProcessingTo] = useState(false);
   const [checkoutError, setCheckoutError] = useState("");
   const [coupan, setCoupan] = useState("");
-  const [publishedKey ,setPublishedKey] = useState("")
+  const [publishedKey, setPublishedKey] = useState("");
 
   const dispatch = useDispatch();
   const stripe = useStripe();
@@ -52,9 +52,40 @@ function Address({ setShow }) {
       .then((res) => res.json())
       .then((data) => console.log(data));
   });
-  if (!publishedKey){
-    return "Loading......"
-  }
+  // if (!publishedKey){
+  //   return "Loading......"
+  // }
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
