828bc1a995efa31da324f208cedfb35f24d4739c
Samama Islam <Samama.Islam@Biotech.com>
2023-01-06 19:07:08 +0500

    abcd

--- components/Home/Banner.jsx
+++ components/Home/Banner.jsx
@@ -4,13 +4,14 @@ import { useSelector } from "react-redux";
 function Banner() {
 
   const { image } = useSelector((state)=>state.home)
+  console.log("image",image)
   
   return (
     <div className="">
       {/* {image && image?.data?.slice(0,1).map((items ,index) => { */}
         {/* return */}
          <img className="lg:hidden w-full " src={image?.data[0]?.image} alt="" />
-         <img className=" hidden lg :block md:h-[100vh] w-full " src={image?.data[1]?.image} alt="" />
+         <img className=" hidden  lg:block md:h-[100vh] w-full " src={image?.data[1]?.image} alt="" />
          {/* <img className="  md:h-[100vh] w-full " src={image?.data?.image[1]} alt="" /> */}
       {/* })} */}
     </div>
