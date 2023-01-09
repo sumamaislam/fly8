5cbb2f797efe0e2f693608f7fad34937cf7872e7
Hammad Nadir <Hammad.Nadir@Biotech.com>
2023-01-09 18:23:46 +0500

    okkk

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -15,15 +15,15 @@ function Profile() {
   const [show, setShow] = useState("orders");
   return (
     <div>
-        <Header />
-      
-        <Account show={show} setShow={setShow} />
-        {show === "address" && <Form />}
-        {show === "account" && <User/>}
-        {show === "payment" &&<PaymentCard />}
-        {show === "subscription" && <Subscription />}
-        <Email />
-        <Footer />
+      <Header />
+
+      <Account show={show} setShow={setShow} />
+      {show === "address" && <Form />}
+      {show === "account" && <User />}
+      {show === "payment" && <PaymentCard />}
+      {show === "subscription" && <Subscription />}
+      <Email />
+      <Footer />
     </div>
   );
 }
@@ -35,18 +35,18 @@ export const getServerSideProps = wrapper.getServerSideProps((store) => async (c
   const session = await getSession({ req: context.req });
   await store.dispatch(navDataRequest());
   await store.dispatch(footerDataRequest());
-  // if (session) {
-  //   return {
-  //     props: {
-  //       session,
-  //     },
-  //   };
-  // } else {
-  //   return {
-  //     redirect: {
-  //       destination: `/login`,
-  //       permanent: false,
-  //     },
-  //   };
-  // }
+  if (session) {
+    return {
+      props: {
+        session,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: `/login`,
+        permanent: false,
+      },
+    };
+  }
 });
